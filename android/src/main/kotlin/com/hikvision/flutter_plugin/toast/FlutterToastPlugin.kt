fe0f12f883c1b904d80edc6930f32642c2ea6d0a
Jimmuy <cool_tang123@163.com>
2020-03-31 16:19:48 +0800

    update

--- android/src/main/kotlin/com/hikvision/flutter_plugin/toast/FlutterToastPlugin.kt
+++ android/src/main/kotlin/com/hikvision/flutter_plugin/toast/FlutterToastPlugin.kt
@@ -14,7 +14,7 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** FluttertoastPlugin  */
-open class FlutterToastPlugin private constructor(private val ctx: Context) : MethodCallHandler {
+open class FlutterToastPlugin constructor(private val ctx: Context) : MethodCallHandler {
 
     companion object {
         /** Plugin registration.  */
